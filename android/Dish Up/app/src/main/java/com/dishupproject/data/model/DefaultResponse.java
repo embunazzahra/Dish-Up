ccc4decd339b4d1354a36a293ac5685639276194
zananiswah <zana.niswah@ui.ac.id>
2022-11-23 21:10:46 +0700

    Modifikasi Login Register

--- android/Dish Up/app/src/main/java/com/dishupproject/data/model/DefaultResponse.java
+++ android/Dish Up/app/src/main/java/com/dishupproject/data/model/DefaultResponse.java
@@ -1,6 +1,22 @@
 package com.dishupproject.data.model;
 
-public class DefaultResponse {
+import com.google.gson.annotations.SerializedName;
 
+/**
+ * Class that represents the response in general
+ *
+ * @author Dhau' Embun Azzahra
+ */
+public class DefaultResponse {
+    @SerializedName("message")
+    private String message;
+
+    public DefaultResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 
 }
--- android/Dish Up/app/src/main/java/com/dishupproject/data/model/GetRecipesResponse.java
+++ android/Dish Up/app/src/main/java/com/dishupproject/data/model/GetRecipesResponse.java
@@ -1,5 +1,33 @@
 package com.dishupproject.data.model;
 
-public class GetRecipesResponse extends  DefaultResponse{
+import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+/**
+ * Class that represents the response in the
+ * form of recipes list
+ *
+ * @author Dhau' Embun Azzahra
+ */
+public class GetRecipesResponse extends  DefaultResponse{
+    @SerializedName("data")
+    private List<Recipe> recipes = null;
+
+    public GetRecipesResponse(String message, List<Recipe> recipes) {
+        super(message);
+        this.recipes = recipes;
+    }
+
+    public GetRecipesResponse(String message){
+        super(message);
+    }
+
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
 }
